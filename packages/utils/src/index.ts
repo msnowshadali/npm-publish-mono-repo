57bd397a401efae0c1fc50d9ded6f2052a471c03
Noushad Ali <ms.nowshadali@gmail.com>
2025-08-20 01:29:49 +0530

    feat: update utils package version to 1.0.1 for new release

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,4 +1,4 @@
-// Utils Package - Sample Utility Functions
+// Utils Package - Sample Utility Functions - Updated for release
 export * from './string';
 export * from './string-count';
 export * from './array';
@@ -6,5 +6,5 @@ export * from './object';
 export * from './date';
 export * from './math';
 
-// Version info
-export const VERSION = '0.0.0-development';
+// Version info - Updated for new release
+export const VERSION = '1.0.1';
